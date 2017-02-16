1e4009d126eb90ecac2dcdbb88c48d9a441becb9
Byron Quezada <byron.quezada@e-campus.uab.cat>
2017-02-17 00:49:00 +0100

    Added numpy exercise 1

---
+++ a-Introduction/numpy1-arrays.py
@@ -0,0 +1,18 @@
+# Import Numpy
+import numpy as np
+
+# Creating a vector (1D array)
+B = np.array([10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+print(B)
+
+# To get help uncomment
+# help(np.array)
+
+# Function len() returns the size of the first dimension
+print(len(B))
+
+# Method to get its shape
+print(B.shape)
+
+# Method to get the number of dimensions of an array
+print(B.ndim)
