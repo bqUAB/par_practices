1d820efed163a00f0558b23f74bf1a3a568ecc92
SocialDesigner <yalim.erdem@gmail.com>
2017-02-24 11:57:41 +0100

    Updated Gradient Descent...

--- b-text_classification/b1-gradient_descent.py
+++ b-text_classification/b1-gradient_descent.py
@@ -50,8 +50,87 @@ with open('./PR1data1.pkl', 'rb') as f:
 print X.shape, y.shape
 
 # Plot the data
-%matplotlib inline
+# %matplotlib inline
 
+# plt.scatter(X[y == 1, 0], X[y == 1, 1], marker='o', c='b')
+# positive samples
+# plt.scatter(X[y == 0, 0], X[y == 0, 1], marker='x', c='r')
+# negative samples
+# plt.show()
+
+# Append the x_0 column (for the bias term theta_0)
+x = np.ones(shape=(X.shape[0], 1))
+x = np.append(x, X, axis=1)
+
+# Logistic Regression gradient descent optimization
+w = GradientDescent(x, y)
+# Evaluate the classifier accuracy in the training data
+H = [classifyVector(x[i, :], w) for i in range(x.shape[0])]
+print "Training Accuracy : "+str(float(np.sum(H == y)) / y.shape[0])
+
+# Plot data
+# plt.scatter(X[y == 1, 0], X[y == 1, 1], marker='o', c='b')
+# positive samples
+# plt.scatter(X[y == 0, 0], X[y == 0, 1], marker='x', c='r')
+# negative samples
+
+
+# Plot Decision Boundary
+u = np.linspace(-1, 1.5, 50)
+v = np.linspace(-1, 1.5, 50)
+z = np.zeros(shape=(len(u), len(v)))
+for i in range(len(u)):
+    for j in range(len(v)):
+        z[i, j] = sigmoid(np.dot(np.array([1, u[i], v[j]]), w))
+
+z = z.T
+
+# cs = plt.contour(u, v, z, levels=[0.5])
+# plt.clabel(cs, inline=1, fontsize=10)
+
+
+def map_feature(x1, x2):
+    '''
+    Maps 2D features to quadratic features.
+    Returns a new feature vector with more features, comprising of
+    x1, x2, x1^2, x2^2, x1*x2, x1*x2^2, etc...
+    '''
+    x1.shape = (x1.size, 1)
+    x2.shape = (x2.size, 1)
+    degree = 6  # Value to change the complexity default = 6, test = 2
+    out = np.ones(shape=(x1[:, 0].size, 1))
+
+    for i in range(1, degree + 1):
+        for j in range(i + 1):
+            r = (x1 ** (i - j)) * (x2 ** j)
+            out = np.append(out, r, axis=1)
+
+    return out
+
+
+features = map_feature(X[:, 0], X[:, 1])
+features.shape
+
+# Logistic Regression gradient descent optimization
+w = GradientDescent(features, y)
+
+H = [classifyVector(features[i, :], w) for i in range(features.shape[0])]
+print "Training Accuracy : "+str(float(np.sum(H == y)) / y.shape[0])
+
+# Plot data
 plt.scatter(X[y == 1, 0], X[y == 1, 1], marker='o', c='b')  # positive samples
 plt.scatter(X[y == 0, 0], X[y == 0, 1], marker='x', c='r')  # negative samples
+
+# Plot Boundary
+u = np.linspace(np.min(X[:, 0]), np.max(X[:, 0]), 50)
+v = np.linspace(np.min(X[:, 1]), np.max(X[:, 1]), 50)
+z = np.zeros(shape=(len(u), len(v)))
+for i in range(len(u)):
+    for j in range(len(v)):
+        z[i, j] = sigmoid(map_feature(np.array(u[i]), np.array(v[j])).dot(w))
+
+z = z.T
+cs = plt.contour(u, v, z, levels=[0.5])  # levels default = 0.5
+plt.clabel(cs, inline=1, fontsize=10)
+
 plt.show()
