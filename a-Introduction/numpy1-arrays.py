22cbd40559f29d1d8333c10830867e4c94164071
bqUAB <byron.quezada@e-campus.uab.cat>
2017-02-26 01:50:54 +0100

    Finished exercise about len, shape, ndim

--- a-Introduction/numpy1-arrays.py
+++ a-Introduction/numpy1-arrays.py
@@ -1,18 +1,40 @@
 # Import Numpy
 import numpy as np
 
+
 # Creating a vector (1D array)
-B = np.array([10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+A = np.array([10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+print "A =", A
+
+# Create a two dimensional array (2x2 matrix)
+B = np.array([[1, 2], [3, 4]])
+print("B =")
 print(B)
 
-# To get help uncomment
-# help(np.array)
+# Create a two dimensional array that is not a matrix
+C = np.array([[1, 2, 3], [4, 5], [6, 7, 8]])
+print("C =")
+print(C)
 
-# Function len() returns the size of the first dimension
-print(len(B))
 
-# Method to get its shape
-print(B.shape)
+m = "Size of the first dimension (columns) for A ="
+print m, len(A)
+m = "Size of the first dimension (columns) for B ="
+print m, len(B)
+m = "Size of the first dimension (columns) for C ="
+print m, len(C)
 
-# Method to get the number of dimensions of an array
-print(B.ndim)
+m = "Shape of A ="
+print m, A.shape
+m = "Shape of B ="
+print m, B.shape
+m = "Shape of C ="
+print m, C.shape
+
+
+m = "Dimensions of A ="
+print m, A.ndim
+m = "Dimensions of B ="
+print m, B.ndim
+m = "Dimensions of C ="
+print m, C.ndim
